82a916d7bab7c2efeefff98c99ebff9ed2774d70
RajanGadhavi <rajangadhavi5@gmail.com>
2024-11-29 19:09:49 +0530

    Added some featuers and improve functions code is working but .delet is not workig need to change ref

--- main.py
+++ main.py
@@ -17,43 +17,43 @@ ref_users_data = db.reference('Users')
 # "this function is to print list of books" 
 def showAvailableBooks():
     data = ref_available_book.get()
-    for bookid, bookinfo in data.items():
-        print(f"{bookinfo['Title']} by {bookinfo['Author']} and id is {bookid}")
+    for bookid, bookinfo in data['BookData'].items():
+        print(f"{bookinfo['Title']} by {bookinfo['Author']}")
         
+# "Add new user"
+def addNewUser(name, bookUserWant):
+    randomUserId = random.randint(20,100)
+    randomAge = random.randint(20,100)
+    newUser = {
+        f"user{randomUserId}":{
+                "name" : name,
+                "age" : randomAge,
+                "book" : bookUserWant
+            }
+        }
+    ref_users_data.update(newUser)
+    
 # "this function greet user"
 def greetigs(name):
      print(f"Hello!{name} how are you?")
      showAvailableBooks()
 
 #function that handel borrowing book
-def borrowBook(name):
-    bookUserWant = input("Which book do you want ? : ")
+def borrowBook(name,bookUserWant):
     bookFound = False
     data = ref_available_book.get()
-    userData = ref_users_data.get()
     
-    for bookid, bookinfo in data.items():
+    for bookid, bookinfo in data['BookData'].items():
         if bookinfo['Title'] == bookUserWant:
             bookFound = True
-            print(f"Thank you! you have to return {bookid} {bookinfo['Title']} in 30 days")
-            
-            randomUserId = random.randint(20,100)
-            randomAge = random.randint(20,100)
-            newUser = {
-                f"user{randomUserId}":{
-                    "name" : name,
-                    "age" : randomAge
-                }
-            }
-            ref_users_data.update(newUser)
-            
+            print(f"Thank you {name}! you have to return {bookinfo['Title']} in 30 days")
+            addNewUser(name,bookUserWant)           
             ref_available_book.child(f"{bookid}").delete()
             break
         
     if not bookFound:
         print(f"We do not have {bookUserWant} Sorry!")
         
-
 # "this function is for retruning book"
 def returnBook():
     bookUserReturn = input("Which book are you going to return ? : ")
@@ -86,22 +86,33 @@ def returnBook():
                     
 # "main functon" 
 def main():
-    name = input(str("Your name is : "))
-    greetigs(name)
     while True:
         try:
-            userChoice = int(input("Which opreation will you perform ?\n1.Borrow books\n2.Return books\n3.Quit : "))
+            userChoice = int(input("1.Continue\n2.Quite : "))
             if userChoice == 1:
-                borrowBook(name)
+                name = str(input("Please eneter your name : "))
+                print("What would you like to do?")
+                try:
+                    userInput = int(input("1.Borrow Book\n2.Return Book\n3.User Profile\n4.Quite : "))
+                    if userInput == 1:
+                        bookUserWant = input("Which book do you want ? : ")
+                        borrowBook(name,bookUserWant)
+                    elif userInput == 2:
+                        returnBook()
+                    elif userInput == 3:
+                        pass
+                    elif userInput == 4:
+                        break
+                    else:
+                        print("Please type number only 1-4")
+                except ValueError:
+                    print("Please type number only 1,2,3,4")
             elif userChoice == 2:
-                returnBook()
-            elif userChoice == 3:
                 print("Thank you visit again!")
                 break
             else:
                 print("Please type 1,2,3")
         except ValueError:
-            print("Please type number only 1,2,3")
+            print("Please type number only 1,2")
 
-
-borrowBook("name")
+main()
